00148a574863a2e143019efcf0b92e428489f7c9
Ali Ahmadi <mrkiller1383@gmail.com>
2025-02-17 00:16:39 +0330

    Secured user input (name and last name) by disallowing certain characters in register and profile sections.

--- src/schemas/auth/Profile.ts
+++ src/schemas/auth/Profile.ts
@@ -4,11 +4,15 @@ const Profile = z.object({
   firstName: z
     .string({ invalid_type_error: "First name should be of type string" })
     .min(3, { message: "First name should be atleast 3 characters" })
-    .max(20, "First name cannot be more than 20 characters"),
+    .max(20, "First name cannot be more than 20 characters")
+    .regex(/^[a-zA-Z]+$/, { message: "First name can only contain letters" }),
+
   lastName: z
     .string({ invalid_type_error: "Last name should be of type string" })
     .min(3, { message: "Last name should be atleast 3 characters" })
-    .max(20, "Last name cannot be more than 20 characters"),
+    .max(20, "Last name cannot be more than 20 characters")
+    .regex(/^[a-zA-Z]+$/, { message: "Last name can only contain letters" }),
+
   email: z
     .string({ required_error: "Email is required" })
     .email({ message: "Email is not valid" }),
